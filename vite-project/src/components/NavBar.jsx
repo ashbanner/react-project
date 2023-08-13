24f0786ee4f71535e6537a9685c7a40f8e62cbf1
ashbanner <ashleyann016@gmail.com>
2023-08-13 18:52:02 -0500

    add logout button

--- vite-project/src/components/NavBar.jsx
+++ vite-project/src/components/NavBar.jsx
@@ -1,11 +1,34 @@
 import React from "react";
-import { Typography, AppBar, Toolbar } from "@mui/material";
+import { Typography, AppBar, Toolbar, IconButton } from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { useNavigate } from "react-router-dom";
 
 function NavBar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    sessionStorage.removeItem("token");
+    navigate("/login");
+  };
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h4">People Source Co.</Typography>
+        <div style={{ marginLeft: "auto" }}>
+          <IconButton
+            size="large"
+            edge="end"
+            color="inherit"
+            aria-label="Logout"
+            onClick={handleLogout}
+          >
+            <LogoutIcon />
+            <Typography variant="h6" sx={{ marginLeft: 1 }}>
+              Logout
+            </Typography>
+          </IconButton>
+        </div>
       </Toolbar>
     </AppBar>
   );
